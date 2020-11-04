8629b2cec43bc338732d5f26b9b1916d467dfd8c
Yonkata <73932734+YonkataS@users.noreply.github.com>
2020-11-04 10:38:00 +0200

    Add files via upload

---
+++ ESX Joblist with voice 1.0 Yonkata/InteractSound/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+-- Client Scripts
+client_script 'client/main.lua'
+
+-- Server Scripts
+server_script 'server/main.lua'
+
+-- NUI Default Page
+ui_page('client/html/index.html')
+
+-- Files needed for NUI
+-- DON'T FORGET TO ADD THE SOUND FILES TO THIS!
+files {
+    'client/html/index.html',
+    -- Begin Sound Files Here...
+    -- client/html/sounds/ ... .ogg
+    'client/html/sounds/ayuntamiento.ogg', 
+    'client/html/sounds/oficina.ogg'
+} 
